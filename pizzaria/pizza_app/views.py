10623310123cc9ebec29501edbf4c76d80c4cb71
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2025-03-14 20:40:51 -0300

    últimas alterações antes da aula de sábado

--- pizzaria/pizza_app/views.py
+++ pizzaria/pizza_app/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    lista_produtos = [
+        {
+            'img': 'http://',
+            'nome': 'pizza 4 queijos',
+            'ingredientes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ 90,00',
+        },
+        {
+            'img': 'http://',
+            'nome': 'pizza de boldo',
+            'ingredientes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ 66,00',
+        },
+        {
+            'img': 'http://',
+            'nome': 'pizza 4 carqueja',
+            'ingredientes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ 60,00',
+        },
+    ]
+    return render(request, "pizza_app/pages/home.html", context={'produtos': lista_produtos})
 # def sobre(request):
 #     return render(request, 'sobre.html')
