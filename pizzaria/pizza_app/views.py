0e372ff587ec6d2bd52ca06b74c77c790a0d60e2
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2025-03-25 16:04:16 -0300

    alterações revisão aula 5

--- pizzaria/pizza_app/views.py
+++ pizzaria/pizza_app/views.py
@@ -3,23 +3,42 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     lista_produtos = [
+        
         {
-            'img': 'http://',
-            'nome': 'pizza 4 queijos',
-            'ingredientes': 'huauhauh, huahua, ppipipi',
-            'preco': 'R$ 90,00',
+            'img': 'https://w3c.br/wp-content/uploads/2010/09/Prancheta-1.png',
+            'nome': 'HTML',
+            'detalhes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ ??,??',
+        },
+          {
+            'img': 'https://www.stickersdevs.com.br/wp-content/uploads/2015/03/css3-stickers-adesivo.png',
+            'nome': 'CSS',
+            'detalhes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ ??,??',
+        },
+          {
+            'img': 'https://devblogs.microsoft.com/python/wp-content/uploads/sites/12/2018/08/pythonfeature.png',
+            'nome': 'Python',
+            'detalhes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ 00,00',
         },
         {
-            'img': 'http://',
-            'nome': 'pizza de boldo',
-            'ingredientes': 'huauhauh, huahua, ppipipi',
-            'preco': 'R$ 66,00',
+            'img': 'https://media.geeksforgeeks.org/wp-content/uploads/20200210175202/django-basics.png',
+            'nome': 'Django',
+            'detalhes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ 00,00',
         },
         {
-            'img': 'http://',
-            'nome': 'pizza 4 carqueja',
-            'ingredientes': 'huauhauh, huahua, ppipipi',
-            'preco': 'R$ 60,00',
+            'img': 'https://saidvandeklundert.net/img/jinja_logo.png',
+            'nome': 'Jinja',
+            'detalhes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ 00,00',
+        },
+        {
+            'img': 'https://user-images.githubusercontent.com/51070104/268566349-c41e65a5-2ab9-4b54-8cbc-350ab6da746c.png',
+            'nome': 'Flask',
+            'detalhes': 'huauhauh, huahua, ppipipi',
+            'preco': 'R$ ??,??',
         },
     ]
     return render(request, "pizza_app/pages/home.html", context={'produtos': lista_produtos})
